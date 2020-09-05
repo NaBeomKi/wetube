ff9809d445ea40267da0c69ff055190600ca331a
Na Beomki <beomki69@gmail.com>
2020-09-05 17:08:33 +0900

    add Controllers

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import globalRouter from "./routers/globalRouter.js";
-import userRouter from "./routers/userRouter.js";
-import videoRouter from "./routers/videoRouter.js";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const { request } = require("http");
 const app = express();
@@ -17,8 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
