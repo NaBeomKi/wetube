bf1c4979b3f8dc5bc86298cba448c859b6494846
Na Beomki <beomki69@gmail.com>
2020-09-05 14:41:14 +0900

    add routers

---
+++ app.js
@@ -0,0 +1,24 @@
+// const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+
+const { request } = require("http");
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
+
+export default app;
