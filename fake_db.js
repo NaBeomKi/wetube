cc3cd613d1f60e7a4de320117d79b0bac810f297
Na Beomki <beomki69@gmail.com>
2020-09-08 20:42:06 +0900

    MongoDB and Mongoose

---
+++ fake_db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12345,
+      name: "Ki",
+      email: "nabeomki@las.com",
+    },
+  },
+  {
+    id: 654786,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12345,
+      name: "Ki",
+      email: "nabeomki@las.com",
+    },
+  },
+  {
+    id: 348678,
+    title: "Video power",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12345,
+      name: "Ki",
+      email: "nabeomki@las.com",
+    },
+  },
+  {
+    id: 954675,
+    title: "Video hero",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12345,
+      name: "Ki",
+      email: "nabeomki@las.com",
+    },
+  },
+];
