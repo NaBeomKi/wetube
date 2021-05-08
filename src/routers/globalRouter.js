967f04462d6f32048145ee443728ac4c1da8133e
beomki69 <beomki69@gmail.com>
2021-05-08 23:35:14 +0900

    add controllers

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { trending } from "../controllers/videoController";
+import { join } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
 
-export default globalRouter.get("/", handleHome);
+export default globalRouter;
