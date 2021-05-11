f3193aa4d6e3110d68d0f18251ec8794825ffb72
beomki69 <beomki69@gmail.com>
2021-05-11 19:40:19 +0900

    add mixins, mvp.css, urlencoded

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,57 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+const videos = [
+  {
+    title: "hello",
+    rating: 5,
+    comments: 10,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "video 2",
+    rating: 5,
+    comments: 10,
+    createdAt: "2 minutes ago",
+    views: 65,
+    id: 2,
+  },
+  {
+    title: "im ki",
+    rating: 5,
+    comments: 10,
+    createdAt: "2 minutes ago",
+    views: 65,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+export const watch = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = videos[id - 1];
+  return res.render("editVideo", {
+    pageTitle: `Editing: ${video.title}`,
+    video,
+  });
+};
+export const postEdit = (req, res) => {
+  const {
+    params: { id },
+    body: { title },
+  } = req;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
