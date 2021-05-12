bb4f67d3a219c7cb14f0e3b1d6a745f4883a0f32
beomki69 <beomki69@gmail.com>
2021-05-12 14:48:16 +0900

    add uploadVideo, mongoDB, mongoose

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,14 +2,15 @@ import express from "express";
 import {
   watch,
   getEdit,
-  upload,
+  getUpload,
   deleteVideo,
   postEdit,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
