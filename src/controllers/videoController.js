41734963c00f820449ac7827b2964ae39f225e79
beomki69 <beomki69@gmail.com>
2021-05-31 01:44:17 +0900

    use populate

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,12 +9,11 @@ export const watch = async (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = await Video.findById(id);
-  const owner = await User.findById(video.owner);
+  const video = await Video.findById(id).populate("owner");
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
-  return res.render("watch", { pageTitle: video.title, video, owner });
+  return res.render("watch", { pageTitle: video.title, video });
 };
 export const getEdit = async (req, res) => {
   const {
