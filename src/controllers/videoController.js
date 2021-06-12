1c5305bdfa17aa4bd696cf99383060093e972352
beomki69 <beomki69@gmail.com>
2021-06-12 13:39:07 +0900

    Add Comment Function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   const videos = await Video.find({})
@@ -12,7 +13,7 @@ export const watch = async (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
@@ -141,4 +142,38 @@ export const registerView = async (req, res) => {
   video.meta.views += 1;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { text },
+    session: {
+      user: { _id: userId },
+    },
+  } = req;
+
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  const comment = await Comment.create({
+    text,
+    owner: userId,
+    video: id,
+  });
+
+  video.comments.push(comment._id);
+  video.save();
+
+  user.comments.push(comment._id);
+  user.save();
+
+  return res.status(201).json({ newCommentId: comment._id });
 };
